6a22bebd5a01ff48cae9e82e38405a2adab5941b
panecatl <ypanecatl24@gmail.com>
2022-05-14 19:46:07 -0500

    working file, added random and dunction

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,100 @@
+
+var passwordLength;
+var pickedLowercaseLetters;
+var pickedUppercaseLetters;
+var pickedNumbers;
+var pickedSpecialCharacters;
+
+lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y" ,"z"];
+uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "'", ",", ".", "<", ">", "?", "/"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+
+// where final choices will be stored
+var confirmChoices; 
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 /* Need to prompt user for password
-  a. password length 8-128 characters 
-  b. lowercase, uppercase, numbers, special characters */
-  window.prompt("How many characters would you like your password to contain?");
+  a. password length 8-128 characters */ 
+  var passwordLength = window.prompt("How many characters would you like your password to contain? Pick between 8 and 128.");
+
   //need a way to verify that the password length is between 8-128
-  window.confirm("Do you want to include lowercase letters?");
-  //confirm if user selcted yes or no
-  window.confirm("Do you want to include uppercase letters?");
-  //confirm if user selcted yes or no
-  window.confirm("Do you want to include numbers?");
-  //confirm if user selcted yes or no
-  window.confirm("Do you want to include special characters?");
-  //confirm if user selcted yes or no
+  if (!passwordLength) {
+    window.alert("Make sure you enter pickedNumbers 8 up to 128");
+  }
+  else if (passwordLength <= 8 || passwordLength >= 128) {
+    window.alert ("Plase make sure you are only using between 8-128 characters!");
+  } else {
+    pickedLowercaseLetters = window.confirm("Do you want to include lower case letters?");
+    pickedUppercaseLetters = window.confirm("Do you want to include upper case letters?");
+    pickedNumbers = window.confirm("Do you want to include pickedNumbers?");
+    pickedSpecialCharacters = window.confirm("Do you want to include special characters?"); 
+  };
+
+  //if user selected no for everything 
+  if (!pickedLowercaseLetters && !pickedUppercaseLetters && !pickedNumbers && !pickedSpecialCharacters){
+    confirmChoices = alert("Please make sure you are selecting more than two criteria!");
+  }
+  else if (pickedLowercaseLetters && pickedUppercaseLetters && pickedNumbers && pickedSpecialCharacters) {
+    confirmChoices = lowercase.concat(uppercase, specialCharacters, numbers);
+  }
+
+  //if user only selects three choices
+  //starting with lowercase
+  else if (pickedLowercaseLetters && pickedUppercaseLetters && pickedNumbers) {
+    confirmChoices = lowercase.concat(uppercase, numbers);
+  } 
+  else if (pickedLowercaseLetters && pickedUppercaseLetters && pickedSpecialCharacters) {
+    confirmChoices = lowercase.concat(uppercase, specialCharacters)
+  }
+  else if (pickedLowercaseLetters && pickedNumbers && pickedSpecialCharacters) {
+    confirmChoices = lowercase.concat(numbers, specialCharacters)
+  }
+  //uppercase selections
+  else if (pickedUppercaseLetters && pickedNumbers && pickedSpecialCharacters) {
+    confirmChoices = uppercase.concat(numbers, specialCharacters)
+  }
+
+  //If user only selects two choices
+  else if (pickedLowercaseLetters && pickedUppercaseLetters) {
+    confirmChoices = lowercase.concat(uppercase)
+  }
+  else if (pickedLowercaseLetters && pickedNumbers) {
+    confirmChoices = lowercase.concat(numbers)
+  }
+  else if(pickedLowercaseLetters && pickedSpecialCharacters) {
+    confirmChoices = lowercase.concat(specialCharacters)
+  }
+  else if (pickedUppercaseLetters && pickedNumbers) {
+    confirmChoices = uppercase.concat(numbers)
+  }
+  else if (pickedUppercaseLetters && pickedSpecialCharacters) {
+    confirmChoices = uppercase.concat(specialCharacters)
+  }
+  else if (pickedNumbers && pickedSpecialCharacters) {
+    confirmChoices = numbers.concat(specialCharacters)
+  }
+};
+
+var password = [];
+
+for (var i=0; i < enter; i++) {
+  var completedChoices = confirmChoices[Math.floor(Math.random() * confirmChoices.length)];
+  password.push(completedChoices); 
+}
+
+
+  
 
 
 //generate password criteria
 
 //Display generated password on page 
   //return "Your password is " + password;
-};
+
 
 // Write password to the #password input
 function writePassword() {
